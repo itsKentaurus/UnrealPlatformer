c9b43326338bf8cbaa6b2f86e9dab530641b0357
itsKentaurus <rigil44@gmail.com>
2023-04-15 12:06:06 -0400

    Creating Modules

--- Platform/Source/PlatformEditor.Target.cs
+++ Platform/Source/PlatformEditor.Target.cs
@@ -11,5 +11,11 @@ public class PlatformEditorTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("Platform");
+		RegisterModulesCreatedByRider();
+	}
+
+	private void RegisterModulesCreatedByRider()
+	{
+		ExtraModuleNames.AddRange(new string[] { "Shared", "Gameplay", "UI", "Shared", "UI", "Gameplay" });
 	}
 }
---
+++ Platform/Source/Shared/Private/Shared.cpp
@@ -0,0 +1,17 @@
+﻿#include "Shared.h"
+
+#define LOCTEXT_NAMESPACE "FSharedModule"
+
+void FSharedModule::StartupModule()
+{
+    
+}
+
+void FSharedModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FSharedModule, Shared)
